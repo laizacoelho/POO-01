1a71d2c4533322d8d7fabf884d15f4809a91d16d
Laíza Coelho <laiza.coelho@gmail.com>
2022-05-09 15:02:07 -0300

    Exercício01 finalizado

--- src/Aula02/Exercicio01.java
+++ src/Aula02/Exercicio01.java
@@ -4,49 +4,72 @@ import java.util.Date;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        Date aniversario01 = new Date(1996,5,8);
-        Jogador jogador01 = new Jogador(1, "Claudio Taffarel", "Taffarel", aniversario01,1,"goleiro",100,0,0, false);
+        Date aniversario01 = new Date(96,4,8);
+        Jogador jogador01 = new Jogador(1, "Claudio Taffarel", "Taffarel", aniversario01,1,"Goleiro",100,0,0, false);
+        jogador01.verificarCondicaoDeJogo();
+
+        Date aniversario02 = new Date(64,7,17);
+        Jogador jogador02 = new Jogador(2,"Jorge de Amorim", "Jorginho", aniversario02, 2, "Lateral direito", 100, 0,0, false);
+        jogador02.verificarCondicaoDeJogo();
+
+        Date aniversario03 = new Date(69,8,15);
+        Jogador jogador03 = new Jogador(3, "Márcio Santos", "Márcio Santos", aniversario03, 15, "Zagueiro", 100, 0,0,false);
+        jogador03.verificarCondicaoDeJogo();
+
+        Date aniversario04 = new Date(62,8,11);
+        Jogador jogador04 = new Jogador(4,"Ricardo Rocha", "Ricardo Rocha", aniversario04, 3, "Zagueiro", 100,0,0,false);
+        jogador04.verificarCondicaoDeJogo();
+
+        Date aniversario05 = new Date(69,8,5);
+        Jogador jogador05 = new Jogador(5, "Leonardo Nascimento", "Leonardo", aniversario05, 16, "Lateral esquerdo", 100,0,1,false);
+        jogador05.verificarCondicaoDeJogo();
+
+        Date aniversario06 = new Date(68,0,12);
+        Jogador jogador06 = new Jogador(6, "Mauro Silva", "Mauro Silva", aniversario06, 5, "Volante", 100, 0,0, false);
+        jogador06.verificarCondicaoDeJogo();
+
+        Date aniversario07 = new Date(63,9,31);
+        Jogador jogador07 = new Jogador(7, "Carlos Verri", "Dunga", aniversario07, 8,"Volante", 100, 0,0,false);
+        jogador07.verificarCondicaoDeJogo();
+
+        Date aniversario08 = new Date(67,5,17);
+        Jogador jogador08 = new Jogador(8, "Crizam Filho", "Zinho", aniversario08, 9, "Meio campo", 100,0,0,false);
+        jogador08.verificarCondicaoDeJogo();
+
+        Date aniversario09 = new Date(65,4,15);
+        Jogador jogador09 = new Jogador(9, "Raí de Oliveira", "Raí", aniversario09, 10, "Meio campo", 100,0,0, false);
+        jogador09.verificarCondicaoDeJogo();
+
+        Date aniversario10 = new Date(64,1,16);
+        Jogador jogador10 = new Jogador(10, "José Roberto", "Bebeto", aniversario10, 7, "Atacante", 100,0,0, false);
+        jogador10.verificarCondicaoDeJogo();
+
+        Date aniversario11 = new Date(66,0,29);
+        Jogador jogador11 = new Jogador(11, "Romário de Souza", "Romário", aniversario11, 11, "Centroavante", 100,0,0,false);
+        jogador11.verificarCondicaoDeJogo();
+
+        jogador04.aplicarCartaoAmarelo(3);
+
+        jogador02.aplicarCartaoVermelho();
+        jogador02.cumprirSuspencao();
+
+        jogador10.sofrerLesao();
+        jogador10.executarTreinamento();
+        jogador10.executarTreinamento();
+
+        jogador11.aplicarCartaoAmarelo(2);
+        jogador11.aplicarCartaoAmarelo(1);
+
         System.out.println(jogador01);
-
-        Date aniversario02 = new Date(1964,8,17);
-        Jogador jogador02 = new Jogador(2,"Jorge de Amorim", "Jorginho", aniversario02, 2, "lateral direito", 100, 0,0, false);
         System.out.println(jogador02);
-
-        Date aniversario03 = new Date(1969,9,15);
-        Jogador jogador03 = new Jogador(3, "Márcio Santos", "Márcio Santos", aniversario03, 15, "zagueiro", 100, 0,0,false);
         System.out.println(jogador03);
-
-        Date aniversario04 = new Date(1962,9,11);
-        Jogador jogador04 = new Jogador(4,"Ricardo Rocha", "Ricardo Rocha", aniversario04, 3, "zagueiro", 100,0,0,false);
         System.out.println(jogador04);
-
-        Date aniversario05 = new Date(1969,9,5);
-        Jogador jogador05 = new Jogador(5, "Leonardo Nascimento", "Leonardo", aniversario05, 16, "lateral esquerdo", 100,0,0,false);
         System.out.println(jogador05);
-
-        Date aniversario06 = new Date(1968,1,12);
-        Jogador jogador06 = new Jogador(6, "Mauro Silva", "Mauro Silva", aniversario06, 5, "volante", 100, 0,0, false);
         System.out.println(jogador06);
-
-        Date aniversario07 = new Date(1963,10,31);
-        Jogador jogador07 = new Jogador(7, "Carlos Verri", "Dunga", aniversario07, 8,"volante", 100, 0,0,false);
         System.out.println(jogador07);
-
-        Date aniversario08 = new Date(1967,6,17);
-        Jogador jogador08 = new Jogador(8, "Crizam Filho", "Zinho", aniversario08, 9, "meio campo", 100,0,0,false);
         System.out.println(jogador08);
-
-        Date aniversario09 = new Date(1965,5,15);
-        Jogador jogador09 = new Jogador(9, "Raí de Oliveira", "Raí", aniversario09, 10, "meio campo", 100,0,0, false);
         System.out.println(jogador09);
-
-        Date aniversario10 = new Date(1964,2,16);
-        Jogador jogador10 = new Jogador(10, "José Roberto", "Bebeto", aniversario10, 7, "atacante", 100,0,0, false);
         System.out.println(jogador10);
-
-        Date aniversario11 = new Date(1966,1,29);
-        Jogador jogador11 = new Jogador(11, "Romário de Souza", "Romário", aniversario11, 11, "centroavante", 100,0,0,false);
         System.out.println(jogador11);
-
     }
 }
--- src/Aula02/Jogador.java
+++ src/Aula02/Jogador.java
@@ -1,6 +1,9 @@
 package Aula02;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 class Jogador {
     int id;
@@ -13,6 +16,7 @@ class Jogador {
     int cartoesAmarelos;
     int cartaoVermelho;
     Boolean suspenso;
+    Boolean jaTreinou = false;
 
     Jogador() {
 
@@ -27,23 +31,71 @@ class Jogador {
         this.numero = numero;
         this.posicao = posicao;
         this.qualidade = qualidade;
-        this. cartoesAmarelos = cartoesAmarelos;
+        this.cartoesAmarelos = cartoesAmarelos;
         this.cartaoVermelho = cartaoVermelho;
         this.suspenso = suspenso;
     }
 
     @Override
     public String toString() {
+        String status = suspenso ? "NÃO PODE JOGAR" : "PODE JOGAR";
+        String formato = "yyyy-MM-dd";
+        DateFormat dataFormatada = new SimpleDateFormat(formato);
+        String data = dataFormatada.format(dataNascimento);
+
         return posicao + ": " + numero + " - " + nome + " (" + apelido + ")" +
-                " - " + dataNascimento + " CONDIÇÃO: PODE JOGAR";
+                " - " + data + " CONDIÇÃO: " + status;
         // FAZER AUMTOMÁTICO A CONDIÇÃO DE JOGO e acertar a impressão da data
     }
 
     boolean verificarCondicaoDeJogo() {
-
-
-        return false;
+        suspenso = cartaoVermelho >= 1 || cartoesAmarelos > 2;
+        return suspenso;
     }
 
+    void aplicarCartaoAmarelo(int quantidade) {
+        cartoesAmarelos += quantidade;
+        if (cartoesAmarelos > 2) {
+            suspenso = true;
+        }
+    }
 
+    void aplicarCartaoVermelho () {
+        cartaoVermelho = 1;
+        suspenso = true;
+    }
+
+    void cumprirSuspencao() {
+        cartaoVermelho = 0;
+        cartoesAmarelos = 0;
+        suspenso = false;
+        System.out.printf("%s cumpriu suspensão e está apto a jogar novamente\n", apelido);
+    }
+
+    void sofrerLesao() {
+        Random rand = new Random();
+        int lesao = rand.nextInt(100) + 1;
+        if (lesao <= 40) {
+            qualidade--;
+        } else if (lesao <= 70) {
+            qualidade -= 2;
+        } else if (lesao <= 85) {
+            qualidade *= 0.95;
+        } else if (lesao <= 95) {
+            qualidade *= 0.9;
+        } else {
+            qualidade *= 85;
+        }
+    }
+
+    void executarTreinamento() {
+       if (!jaTreinou) {
+           Random rand = new Random();
+           int treinar = rand.nextInt(3) + 1;
+           qualidade += treinar;
+           jaTreinou = true;
+       } else {
+           System.out.printf("%s já treinou antes da patida.\n", apelido);
+       }
+    }
 }
